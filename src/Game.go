c99fcc4970d3a72ea0ce2a291cd54a626c9aedab
5W_lzxEP <5w.lzxep@gmail.com>
2022-07-15 19:07:37 +0200

    saves data into log if player not in match

--- src/Game.go
+++ src/Game.go
@@ -52,6 +52,12 @@ type Teilnehmer2 struct {
 	Tipp int    `json:"Tipp"`
 }
 
+type fail struct {
+	Teilnehmer []Teilnehmer2 `json:"teilnehmer"`
+	MatchId    int64         `json:"matchId"`
+	PlayerId   string        `json:"playerId"`
+}
+
 var bessereDaten map[int][]string
 var config *Config
 var daten map[string]int
@@ -59,8 +65,8 @@ var wettdauer time.Duration
 
 func SetConfig(config2 *Config) {
 	config = config2
-	if config.otp = true; len(*config.Champs) == 0 {
-		config.otp = false
+	if config.otp = false; len(*config.Champs) > 0 {
+		config.otp = true
 		config.champsId = champNamesToId(config.Champs)
 	}
 	wettdauer = time.Duration(config.Wettdauer)
@@ -126,14 +132,49 @@ func Auswertung() {
 			break
 		}
 	}
+
+	// check if dir exist and if not creates it
+	fileInfo, err := os.Stat("results")
+	if err != nil || !fileInfo.IsDir() {
+		//log.Fatal(err)
+		err = os.Mkdir("results", 0750)
+		if err != nil {
+			log.Print("Could not create dir.")
+		}
+	}
+
 	if ind == 11 {
 		log.Println("player not found in result")
 		//log.Fatal("player not found in result")
+
+		t := make([]Teilnehmer2, len(daten))
+		failed := fail{MatchId: aktuellesGame.matchId, PlayerId: aktuellesGame.playerId, Teilnehmer: t}
+
+		index := 0
+		for k, l := range bessereDaten {
+			for _, i := range l {
+				failed.Teilnehmer[index] = Teilnehmer2{Name: i, Tipp: k}
+				index++
+			}
+		}
+
+		file, err := os.Create(fmt.Sprintf("results/error_%d.json", aktuellesGame.matchId))
+		if err != nil {
+			log.Fatal(err)
+		}
+		bites, err := json.MarshalIndent(failed, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Write(bites)
+		log.Printf("player not found in result. But saved tipps in results/error_%s.json\n", aktuellesGame.matchId)
+
 	} else {
 
 		if killd.Info.Participants[ind].TeamEarlySurrendered || killd.Info.Participants[(ind+5)%10].TeamEarlySurrendered {
 			log.Println("Auswertung abgebrochen, da geremaked wurde.")
 			config.TwitchClient.Say(config.Twitchchannel, config.Prefix+" Killspiel wurde abgebrochen, da Remaked wurde.")
+
 		} else {
 			kills := killd.Info.Participants[ind].Kills
 			gewinner := bessereDaten[kills]
@@ -150,15 +191,6 @@ func Auswertung() {
 				}
 			}
 
-			fileInfo, err := os.Stat("results")
-			if err != nil || !fileInfo.IsDir() {
-				//log.Fatal(err)
-				err = os.Mkdir("results", 0750)
-				if err != nil {
-					log.Print("Could not create dir.")
-				}
-			}
-
 			file, err := os.Create(fmt.Sprintf("results/%d.json", aktuellesGame.matchId))
 			if err != nil {
 				log.Fatal(err)
@@ -168,7 +200,7 @@ func Auswertung() {
 				log.Fatal(err)
 			}
 			file.Write(bites)
-			log.Printf("Ergebnis gespeichert in results/%d.json", aktuellesGame.matchId)
+			log.Printf("Ergebnis gespeichert in results/%d.json\n", aktuellesGame.matchId)
 
 			var haben string
 			switch len(gewinner) {
@@ -186,7 +218,7 @@ func Auswertung() {
 					config.Prefix, config.Lolaccountname, kills, strings.Join(gewinner, ", "), haben))
 		}
 	}
-	daten = make(map[string]int)
+	daten = map[string]int{}
 	bessereDaten = map[int][]string{}
 	config.State = Idle
 }
